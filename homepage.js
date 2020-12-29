26a39ce38ee9c689b22dc5c5cdcaea55b5356e43
Arianahorn <74698341+Arianahorn@users.noreply.github.com>
2020-12-28 16:55:56 -0800

    homepage work

---
+++ homepage.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// scroll to top 
+
+// const btnScrollToTop = document.addEventListener("#btnScrollToTop");
+
+// btnScrollToTop.addEventListener("click", function () {
+    // window.scrollTo(0, 0);
+
+    // windouw.scrollTo({
+    //      top: 0,
+    //      left: 0,
+    //     behavior: "smooth"
+    // });
+
+//     $("html, body").animate({ scrollTop: 0 }, "slow");
+// })
+
+  
