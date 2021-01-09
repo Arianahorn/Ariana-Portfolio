2fac47b39908e042681d75f611af447325b53992
Arianahorn <74698341+Arianahorn@users.noreply.github.com>
2021-01-09 10:48:03 -0800

    ..

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,6 @@
+console.log("testing")
+
+// console.log( $(".item2 h3") )
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -8,25 +11,9 @@ function myFunction() {
   }
 
   // scroll text in 
+// $('.item2 p').animate({
+//   left: "500px",
+//   opacity: "0.3"
+// })
 
-type = "text/javascript">
-$(document).ready(function(){
-  //Take your div into one js variable
-  var div = $("#divToShowHide");
-  //Take the current position (vertical position from top) of your div in the variable
-  var pos = div.position();
-  //Now when scroll event trigger do following
-  $(window).scroll(function () {
-   var windowpos = $(window).scrollTop();
-   //Now if you scroll more than 100 pixels vertically change the class to AfterScroll
-   // I am taking 100px scroll, you can take whatever you need
-   if (windowpos >= (pos.top - 100)) {
-     div.addClass("AfterScroll");
-   }
-   //If scroll is less than 100px, remove the class AfterScroll so that your content will be hidden again 
-   else {
-     s.removeClass("AfterScroll");
-   }
-   //Note: If you want the content should be shown always once you scroll and do not want to hide it again when go to top agian, no need to write the else part
- });
-});
+
